b9a5a4ea31d8015c223e51e0d44dcf82a28f8588
Make-Peace465 <190812868+Make-Peace465@users.noreply.github.com>
2025-05-01 22:25:14 +0800

    Finish the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let answer = "";
+    if (num < 0) {
+        return 'ERROR';
+    } else if (num === 0) {
+        return answer;
+    } else {
+        for (i = 1; i <= num; i++) {
+            answer = answer.concat(string)
+        }
+    }
+    return answer;
 };
 
 // Do not edit below this line
