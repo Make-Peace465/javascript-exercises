f2b2c9f50f28d64c8ce384cd5020748143bcfc14
Make-Peace465 <190812868+Make-Peace465@users.noreply.github.com>
2025-05-02 16:26:11 +0800

    Finish the exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr, ...num) {
+    let newArray = [];
+    for (const item of arr) {
+      for (const num2 of num) {
+        if (item !== num2) {
+          newArray = newArray.push(arr[num2-1]);
+        }
+      }
+    }
+    return newArray;
+  };
+  
+  console.log(removeFromArray([1, 2, 3, 4], 3, 2));
+  
 
 // Do not edit below this line
 module.exports = removeFromArray;
