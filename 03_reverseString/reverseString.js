d1131c278afc6d84bf06c3c178d0d56428a64741
Make-Peace465 <190812868+Make-Peace465@users.noreply.github.com>
2025-05-02 10:03:17 +0800

    Finish the exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = [];
+    string = string.split("");
+    for (i = (string.length-1); i >= 0; i--) {
+        arr.push(string[i]);
+    }
+    arr.join("");
+    return arr;
 };
 
 // Do not edit below this line
