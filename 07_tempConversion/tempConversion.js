db060b991b9b1365b1348b50c08ab4af94776d8f
Make-Peace465 <190812868+Make-Peace465@users.noreply.github.com>
2025-05-05 19:14:03 +0800

    Finish the exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celsiusTemp = 0;
+    celsiusTemp = (fahrenheitTemp - 32)/(9/5);
+    celsiusTemp = Math.round(celsiusTemp * 10)/10;
+  return celsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheitTemp = 0;
+  fahrenheitTemp = (celsiusTemp * (9/5)) + 32;
+  fahrenheitTemp = Math.round (fahrenheitTemp*10)/10;
+return fahrenheitTemp;
 };
 
 // Do not edit below this line
