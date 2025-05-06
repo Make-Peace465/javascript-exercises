482154163c9d652e1e4644bdbc2bb7bf1cd90e5b
Make-Peace465 <190812868+Make-Peace465@users.noreply.github.com>
2025-05-06 18:46:44 +0800

    Finish the exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,20 +8,43 @@ const subtract = function(x, y) {
   return minus;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  let sumValue = 0;
+  for (const item of num) {
+    NumItem = Number(item);
+    sumValue += NumItem;
+  }
+	return sumValue;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let multiplyValue = 0;
+  for (const item of num) {
+    if (num.indexOf(item) == 0) {
+      multiplyValue += item;
+    } else {
+      multiplyValue *= item;
+    }
+  }
+  return multiplyValue;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let powerValue = Math.pow(base, exponent);
+  return powerValue;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorialValue = 1;
+  if (num == 0) {
+    factorialValue = 1;
+    return factorialValue;
+  } else {
+    for (let i = 1; i <= num; i++) {
+      factorialValue *= i;
+    }
+  return factorialValue;
+  }
 };
 
 // Do not edit below this line
