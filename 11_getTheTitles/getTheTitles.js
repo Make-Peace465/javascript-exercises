d229159d8c3a05cb063517c44d8dca7a5eebebd1
Make-Peace465 <190812868+Make-Peace465@users.noreply.github.com>
2025-05-06 15:14:53 +0800

    Finish the exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(booksInfo) {
+    let booksTitleArray = [];
+  //loop through the array
+  for (const books of booksInfo) {
+    //for each item, extract the key-value that includes the title (objectName.keyName)
+    booksTitleArray.push(books.title);   
+  }
+  return booksTitleArray;
 };
 
 // Do not edit below this line
