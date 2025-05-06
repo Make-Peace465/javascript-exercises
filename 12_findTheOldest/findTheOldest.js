74fd435b5d54c57db32b6de0e681e6d4ed11c04a
Make-Peace465 <190812868+Make-Peace465@users.noreply.github.com>
2025-05-06 17:09:57 +0800

    Finish the exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
 const findTheOldest = function() {
+    //Add the death year to object
+    for (const person of people) {
+      if (person.yearOfDeath === undefined) {
+        person.yearOfDeath = 2025;
+      }
+    }
+  
+    //reduce the object to include only the oldest
+    const theOldest = people.reduce((theOldestPerson, currentPerson) => (currentPerson.yearOfDeath - currentPerson.yearOfBirth) > (theOldestPerson.yearOfDeath - theOldestPerson.yearOfBirth) ? currentPerson : theOldestPerson, people[0])
 
-};
+return theOldest.name;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
